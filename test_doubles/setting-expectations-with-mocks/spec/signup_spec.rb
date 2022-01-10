3d3992804fd4482c367b98b7c96f1eafac3c432a
Sergei Palmer <zigpalmer@gmail.com>
2022-01-10 15:14:00 +0000

    Solution

--- test_doubles/setting-expectations-with-mocks/spec/signup_spec.rb
+++ test_doubles/setting-expectations-with-mocks/spec/signup_spec.rb
@@ -4,28 +4,30 @@ require "signup"
 describe Signup do
   describe "#save" do
     it "creates an account with one user" do
+      account = double("account")
+      expect(Account).to receive(:create!).with(name: "Example").and_return(account)
+      expect(User).to receive(:create!).with(account: account, email: "user@example.com")
       signup = Signup.new(email: "user@example.com", account_name: "Example")
-
       result = signup.save
 
-      expect(Account.count).to eq(1)
-      expect(Account.last.name).to eq("Example")
-      expect(User.count).to eq(1)
-      expect(User.last.email).to eq("user@example.com")
-      expect(User.last.account).to eq(Account.last)
       expect(result).to be(true)
     end
   end
 
   describe "#user" do
     it "returns the user created by #save" do
+      account = double("account")
+      user = double("user")
+
+      expect(Account).to receive(:create!).with(name: "Example").and_return(account)
+      expect(User).to receive(:create!).with(account: account, email: "user@example.com").and_return(user)
+
       signup = Signup.new(email: "user@example.com", account_name: "Example")
       signup.save
 
       result = signup.user
 
-      expect(result.email).to eq("user@example.com")
-      expect(result.account.name).to eq("Example")
+      expect(result).to eq(user)
     end
   end
 end
